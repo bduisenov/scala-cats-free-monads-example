625fd740f59adbabbb71d133394217a38d3e7e0b
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-09-02 10:34:53 +0200

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,16 +5,16 @@ object Dependencies {
   val finch: Seq[ModuleID] = Seq(
     "finch-core",
     "finch-circe"
-  ).map("com.github.finagle" %% _ % "0.11.0-M2")
+  ).map("com.github.finagle" %% _ % "0.11.0-M3")
 
   val circe: Seq[ModuleID] = Seq(
     "circe-core",
     "circe-parser",
     "circe-generic"
-  ).map("io.circe" %% _ % "0.5.0-M2")
+  ).map("io.circe" %% _ % "0.5.0")
 
   val cats: Seq[ModuleID] = Seq(
     "cats-core",
     "cats-free"
-  ).map("org.typelevel" %% _ % "0.6.1")
+  ).map("org.typelevel" %% _ % "0.7.2")
 }
